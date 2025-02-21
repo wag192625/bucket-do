b1c560252b84b30be05d09024658ab4309379ed0
성준 <130916219+sungjoon92@users.noreply.github.com>
2025-02-21 20:36:13 +0900

    [BD-62] feat: 로그인, 회원가입 버튼구현 (#13)
    1. Main페이지의 진입시 로그인 여부에 따라
    Home, Login 페이지로 이동하도록 구현완료
    2. 로그인 버튼 클릭시 Main 페이지의 로그인 여부를 props로 받아
    작동하도록 구성 완료(현재는 useState로 test코드를 작성하였으며
    추후에 redux로 받은 값으로 여부설정)
    3. 회원가입 버튼 클릭시 useNavigate로 Signup 페이지로 이동

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from '../styles/Login.module.css';
+import { useNavigate } from 'react-router-dom';
 
-function Login(setisLogin) {
+function Login({ setIsLogin }) {
+  const navigate = useNavigate();
+  const onSignupPage = () => {
+    navigate('/signup'); // '/signup' 페이지로 리다이렉션
+  };
+  const onLogin = () => {
+    setIsLogin(true); // 로그인 버튼을 클릭하면 isLogin을 true로 설정
+  };
+
   return (
     <div className={styles.loginBackGround}>
       <div className={styles.loginContainer}>
         <div className={styles.loginIntro}>
           <div>
-            <img src="/images/BD-logo.png" alt="logo" />
+            <img className={styles.logoImage} src="/images/BD-logo.png" alt="logo" />
           </div>
           <div>
-            <p>버킷두(BucketDo)는 작심삼일러를 위한</p>
-            <p>작은 실천으로 큰 목표를 이루는 계획 서비스 입니다.</p>
+            <p className={styles.introText}>버킷두(BucketDo)는 작심삼일러를 위한</p>
+            <p className={styles.introText}>작은 실천으로 큰 목표를 이루는 계획 서비스 입니다.</p>
           </div>
         </div>
         <div>
@@ -26,11 +35,12 @@ function Login(setisLogin) {
           </form>
         </div>
         <div className={styles.buttonBox}>
-          <button className={styles.loginButton} onClick={() => (setisLogin = true)}>
+          <button className={styles.loginButton} onClick={onLogin}>
             로그인
           </button>
-          {console.log(setisLogin)}
-          <button className={styles.signupButton}>회원가입</button>
+          <button className={styles.signupButton} onClick={onSignupPage}>
+            회원가입
+          </button>
         </div>
       </div>
     </div>
