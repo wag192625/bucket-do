5fa93d4b6896f23a0c4ca6981a936bf2b306441e
wag192625 <106229215+wag192625@users.noreply.github.com>
2025-02-22 15:19:35 +0900

    [BD-71] feat: 전역 CORS 설정 추가 (#26)
    - SecurityConfig 수정 : CORS 설정 추가해서 리액트 5173포트에서 접근 가능하도록 허용
    
    Co-authored-by: steve0312 <steve0312@naver.com>
    Co-authored-by: ssdeok <bechu123456@gmail.com>

--- api/src/main/java/com/example/api/global/config/SecurityConfig.java
+++ api/src/main/java/com/example/api/global/config/SecurityConfig.java
@@ -17,6 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +39,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 연결
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -58,5 +62,18 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
