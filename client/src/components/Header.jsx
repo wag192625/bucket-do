beeba08cc0ec8890f79e5f8e7a08a554f7397b03
subniii <144028057+imdla@users.noreply.github.com>
2025-02-22 14:17:47 +0900

    [BD-32] feat: front 헤더 컴포넌트 (#22)
    * [BD-32] feat: Header 컴포넌트 구조 작성성
    
    - Header (logo, logout button)
    
    * [BD-32] style: Header 컴포넌트 스타일 지정

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styles from '../styles/Header.module.css';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className={styles.container}>
+        <>
+          <h1 className={styles.logo}>
+            <img src="../public/images/BD-logo.png" alt="logo" />
+          </h1>
+        </>
+
+        <>
+          <button className={styles.logoutButton}>로그아웃</button>
+        </>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Login from './Login';
 import Home from './Home';
+import Header from '../components/Header';
 function Main() {
   const [isLogin, setIsLogin] = useState(false);
   // isLogin 은 나중에 redux 로 받은 isLogin값을 받아 화면을 보여줄 예정
-  return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
+  // return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
+  return <Header />;
 }
 
 export default Main;
