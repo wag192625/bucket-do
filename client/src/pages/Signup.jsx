94a1800014e268aac3c3245a9529422e3ffbf7f0
성준 <130916219+sungjoon92@users.noreply.github.com>
2025-02-22 23:22:52 +0900

    [BD-26] feat: front 회원가입 페이지 레이아웃, 스타일, 유효성검사 (#29)
    * [BD-26] feat: 회원가입페이지 레이아웃, 스타일 추가
    
    1. 회원가입 페이지 레이아웃 추가
    2. 회원가입 페이지 스타일 추가
    
    * [BD-26] feat: 회원가입 페이지 레이아웃, 스타일, 유효성 검사
    
    1. 회원가입 페이지 레이아웃 작성
    2. 회원가입 페이지 스타일 작성
    3. 회원가입 페이지 아이디, 이메일, 연락처, 비밀번호 유효성 검사 완료

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,121 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import styles from '../styles/Signup.module.css';
 function Signup() {
-  return <div>Signup</div>;
+  const [id, setId] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [tel, setTel] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordConfirmed, setPasswordConfirmed] = useState(false);
+
+  // id 유효성 검사
+  const idCheck = () => {
+    // if(emailValue === DB에 있는 email 데이터){
+    //   alert("이미 존재하는 이메일입니다")
+    // }else{
+    //   alert("이메일 중복확인 완료!")
+    // }
+  };
+
+  // email 유효성 검사
+  const emailCheck = () => {
+    // if(emailValue === DB에 있는 email 데이터){
+    //   alert("이미 존재하는 이메일입니다")
+    // }else{
+    //   alert("이메일 중복확인 완료!")
+    // }
+  };
+
+  // 비밀번호 확인 메시지 설정
+  let passwordMessage = '';
+  if (passwordCheck) {
+    passwordMessage =
+      password === passwordCheck ? '비밀번호가 일치합니다!' : '비밀번호가 일치하지 않습니다!';
+  }
+
+  const confirm = (e) => {
+    e.priventDefault;
+    if (password != passwordCheck) {
+      alert('비밀번호가 일치하지 않습니다');
+      return;
+    }
+  };
+  return (
+    <div className={styles.signupBackGround}>
+      <div className={styles.signupContainer}>
+        <form className={styles.signupForm}>
+          <div className={styles.DuplicateCheckBox}>
+            <input
+              className={styles.signupInput}
+              type="text"
+              name="id"
+              value={id}
+              placeholder="아이디"
+              required
+              onChange={(e) => setId(e.target.value)}
+            />
+            <button className={styles.emailCheckButton} onClick={emailCheck}>
+              중복 확인
+            </button>
+          </div>
+          <div className={styles.DuplicateCheckBox}>
+            <input
+              className={styles.signupInput}
+              type="email"
+              name="email"
+              value={email}
+              placeholder="이메일"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {/* DB 에 존재하는지 정보 받기 */}
+            <button className={styles.emailCheckButton} onClick={emailCheck}>
+              중복 확인
+            </button>
+          </div>
+
+          <input
+            className={styles.signupInput}
+            type="tel"
+            name="tel"
+            pattern="[0-9]{2,3}[0-9]{3,4}[0-9]{4}"
+            placeholder="연락처 예:01012345678"
+            required
+          />
+          <input
+            className={styles.signupInput}
+            type="text"
+            name="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input
+            className={styles.signupInput}
+            type="text"
+            name="passwordCheck"
+            placeholder="비밀번호 확인"
+            value={passwordCheck}
+            onChange={(e) => setPasswordCheck(e.target.value)}
+            required
+          />
+          <input
+            className={styles.passwordConfirmed}
+            type="text"
+            name="passwordConfirmed"
+            placeholder={passwordMessage} // 상태에 따라 메시지 변경
+            value={passwordMessage}
+            disabled
+          />
+          <button className={styles.signupButton} onClick={(e) => confirm(e)} type="submit">
+            회원가입
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
