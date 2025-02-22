a1a2dd4d689bc55b80f552484bf3c9a0a147b391
subniii <144028057+imdla@users.noreply.github.com>
2025-02-22 15:37:48 +0900

    [BD-33] feature: 푸터 컴포넌트 (#27)
    * [BD-33] feat: Footer 구조 작성
    
    * [BD-33] style: Footer style 지정
    
    - 공통 css 추가

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from '../styles/Footer.module.css';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className={styles.container}>
+        <p>© 2025. BucketDo. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Login from './Login';
 import Home from './Home';
-import Header from '../components/Header';
+import Footer from '../components/Footer';
 function Main() {
   const [isLogin, setIsLogin] = useState(false);
   // isLogin 은 나중에 redux 로 받은 isLogin값을 받아 화면을 보여줄 예정
   // return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
-  return <Header />;
+  return <Footer />;
 }
 
 export default Main;
