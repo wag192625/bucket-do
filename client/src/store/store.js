cb793b9830de566cc25eee15a716392c1dbb3466
subniii <144028057+imdla@users.noreply.github.com>
2025-02-22 12:00:46 +0900

    [BD-66] chore: store 전역 상태 관리 (#19)
    - auth reducer 지정

---
+++ client/src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReduce from "./slices/authSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReduce,
+  },
+});
+
+export default store;
