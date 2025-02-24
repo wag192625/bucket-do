faf2666c89eb93f00176bb56a23800e4ef73f7be
subniii <144028057+imdla@users.noreply.github.com>
2025-02-24 19:42:45 +0900

    [BD-88] test: 로그인 테스트 및 style 수정 완료 (#45)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,57 +1,45 @@
 import React from 'react';
-import styles from '../styles/Login.module.css';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-function Login({ setIsLogin }) {
+import styles from '../styles/Login.module.css';
+
+export default function Login({ setIsLogin }) {
   const navigate = useNavigate();
+
   const handleSignupPage = () => {
-    navigate('/signup'); // '/signup' 페이지로 리다이렉션
+    navigate('/signup');
   };
+
   const handleLogin = () => {
-    setIsLogin(true); // 로그인 버튼을 클릭하면 isLogin을 true로 설정
+    setIsLogin(true);
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.loginContainer}>
+      <>
         <div className={styles.loginIntro}>
-          <div>
-            <img className={styles.logoImage} src="/public/images/BD-logo.png" alt="logo" />
-          </div>
-          <div>
-            <p className={styles.introText}>버킷두(BucketDo)는 작심삼일러를 위한</p>
-            <p className={styles.introText}>작은 실천으로 큰 목표를 이루는 계획 서비스 입니다.</p>
+          <Link to={'/'}>
+            <img className={styles.logoImage} src="../public/images/BD-logo.png" alt="logo" />
+          </Link>
+
+          <div className={styles.introText}>
+            <p>버킷두(BucketDo)는 작심삼일러를 위한</p>
+            <p>작은 실천으로 큰 목표를 이루는 계획 서비스 입니다.</p>
           </div>
         </div>
-        <div>
-          <form className={styles.loginForm}>
-            <input
-              className={styles.emailInput}
-              type="email"
-              name="email"
-              placeholder="이메일"
-              required
-            />
-            <input
-              className={styles.passwordInput}
-              type="password"
-              name="password"
-              placeholder="비밀번호"
-              required
-            />
-            <div className={styles.buttonBox}>
-              <button className={styles.loginButton} onClick={handleLogin}>
-                로그인
-              </button>
-              <button className={styles.signupButton} onClick={handleSignupPage}>
-                회원가입
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      </>
+
+      <>
+        <form className={styles.loginForm}>
+          <input type="text" name="text" placeholder="아이디" required />
+          <input type="password" name="password" placeholder="비밀번호" required />
+
+          <div className={styles.buttonBox}>
+            <button onClick={handleLogin}>로그인</button>
+            <button onClick={handleSignupPage}>회원가입</button>
+          </div>
+        </form>
+      </>
     </div>
   );
 }
-
-export default Login;
