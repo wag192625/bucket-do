187b526e69f7b8a0a89dd434f777b486dfdc4087
subniii <144028057+imdla@users.noreply.github.com>
2025-02-24 10:46:17 +0900

    [BD-29] feat: front 홈 페이지 (#32)
    * [BD-29] chore: App Provider에 store 추가
    
    * [BD-29] refactor: Home 구조 수정
    
    * [BD-29] style: Home 페이지 스타일 지정
    
    * [BD-29] feat: filter 버튼 클릭 시 handleClick 이벤트 추가
    
    * [BD-29] feat: filter에 따른 bucketList 필터링 기능 추가
    
    * [BD-29] feat: bucketList get, bucket create 기능 추가
    
    - api 통한 bucketList get 기능
    - api 통한 bucket create 기능

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-// import { Provider } from 'react-redux';
+
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 export default function App() {
   return (
     <>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
         <RouterProvider router={router}></RouterProvider>
-      {/* </Provider> */}
+      </Provider>
     </>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Header from '../components/Header';
 import BucketList from '../components/BucketList';
 import Footer from '../components/Footer';
+import styles from '../styles/Home.module.css';
 
-function Home() {
+import bucketApi from '../api/bucketApi';
+
+export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [bucketList, setBucketList] = useState([]);
+  const [bucket, setBucket] = useState(null);
+
+  useEffect(() => {
+    const fetchBuckets = async () => {
+      try {
+        const response = await bucketApi.getBuckets();
+        setBucketList(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchBuckets();
+  }, [activeIndex, bucket]);
+
+  const handleCreateBucket = async () => {
+    try {
+      const response = await bucketApi.createBucket();
+      setBucket(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // todo : activeIndex 값으로 BucketList 필터링 위해 BucketList에 전달
+  // todo : 버킷 생성 시 새로 생성된 버킷을 BucketList에 전달
+  // const bucketValue = bucketList.length > 0 ? <BucketList activeIndex={activeIndex} bucket={bucket} /> : <div>버킷리스트를 추가해주세요</div>;
+  const bucketValue = bucketList.length > 0 ? <BucketList /> : <div>버킷리스트를 추가해주세요</div>;
+
+  function handleActiveFilter(index) {
+    setActiveIndex(index);
+  }
+
+  const filterList = ['모두', '진행중', '완료'];
+  const filterButtons = filterList.map((label, index) => (
+    <li key={index}>
+      <button
+        className={activeIndex === index ? `${styles.active}` : ''}
+        onClick={() => handleActiveFilter(index)}
+      >
+        {label}
+      </button>
+    </li>
+  ));
+
   return (
     <div>
-      <Header></Header>
-      <BucketList></BucketList>
-      <Footer></Footer>
+      <Header />
+
+      <section>
+        <div className={styles.container}>
+          <>
+            <ul className={styles.filter}>{filterButtons}</ul>
+          </>
+
+          {/* todo : activeIndex 값으로 BucketList 필터링 */}
+          <div className={styles.bucketList}>{bucketValue}</div>
+        </div>
+      </section>
+
+      <button className={styles.createButton} onClick={handleCreateBucket}>
+        생성
+      </button>
+
+      <Footer />
     </div>
   );
 }
-
-export default Home;
