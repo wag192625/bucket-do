7e674616b10226dc5b1a9be6cf120c71e0ba1048
성준 <130916219+sungjoon92@users.noreply.github.com>
2025-02-24 20:42:41 +0900

    [BD-87] feat: 버킷 컴포넌트, 디자인 추가 (#48)
    - 버킷 컴포넌트 추가
    - 버킷 디자인 추가

--- client/src/components/Bucket.jsx
+++ client/src/components/Bucket.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/bucket.module.css';
 function Bucket() {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <article className={styles.bucketItem}>
-      <form>
-        <input type="image" />
-      </form>
-      <form>
+      {image && (
+        <div className={styles.bucketImageBox}>
+          <img className={styles.bucketImage} src={image} alt="미리보기" />
+        </div>
+      )}
+
+      <form className={styles.bucketForm}>
+        <input
+          className={styles.fileInput}
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
         <input type="text" placeholder="버킷 리스트 내용을 입력해주세요." />
       </form>
+      <div className={styles.buttonBox}>
+        <button className={styles.toogleButton}>V</button>
+        <button className={styles.deleteButton}>X</button>
+      </div>
     </article>
   );
 }
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import Login from './Login';
 import Home from './Home';
 import BucketList from '../components/BucketList';
+import Todo from '../components/Todo';
+
 function Main() {
   const [isLogin, setIsLogin] = useState(false);
   // isLogin 은 나중에 redux 로 받은 isLogin값을 받아 화면을 보여줄 예정
-  return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
-  // return (
-  //   <div>
-  //     <BucketList />
-  //   </div>
-  // );
+  // return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
+  return (
+    <div>
+      <BucketList />
+    </div>
+  );
 }
 
 export default Main;
