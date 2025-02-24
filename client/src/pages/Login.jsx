fefdd6d7b5d1812a446543b0570a2a0429913277
subniii <144028057+imdla@users.noreply.github.com>
2025-02-24 20:41:09 +0900

    [BD-88] feat: 로그인 전역 상태 저장 기능 추가 (#49)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import styles from '../styles/Login.module.css';
+import authApi from '../api/authApi';
+import { login } from '../store/slices/authSlice';
 
 export default function Login({ setIsLogin }) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-  const handleSignupPage = () => {
+  const handleFormInput = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSignup = () => {
     navigate('/signup');
   };
 
-  const handleLogin = () => {
-    setIsLogin(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await authApi.login(formData);
+
+      const username = formData.username;
+      const { token } = response.token;
+      dispatch(login({ token, username }));
+      navigate('/');
+
+      setIsLogin(true);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -30,13 +65,25 @@ export default function Login({ setIsLogin }) {
       </>
 
       <>
-        <form className={styles.loginForm}>
-          <input type="text" name="text" placeholder="아이디" required />
-          <input type="password" name="password" placeholder="비밀번호" required />
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="아이디"
+            required
+            onChange={handleFormInput}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            required
+            onChange={handleFormInput}
+          />
 
           <div className={styles.buttonBox}>
-            <button onClick={handleLogin}>로그인</button>
-            <button onClick={handleSignupPage}>회원가입</button>
+            <button type="submit">로그인</button>
+            <button onClick={handleSignup}>회원가입</button>
           </div>
         </form>
       </>
