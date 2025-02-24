14ba71f44b2ecf626cd29bc57a13775e31adffc3
subniii <144028057+imdla@users.noreply.github.com>
2025-02-24 15:11:33 +0900

    [BD-44] feat: front 투두 컴포넌트 (#37)
    * [BD-44] feat: TodoList 컴포넌트 생성
    
    * [BD-44] feat: Todo 컴포넌트 생성
    
    * [BD-44] style: index.css 삭제
    
    * [BD-44] feat: Todo 컴포넌트 구조 작성
    
    * [BD-44] feat: todo input onChange 함수 추가
    
    * [BD-44] style: todo checkbox 스타일 추가
    
    * [BD-44] feat: delete button 삭제 버튼 스타일 추가
    
    * [BD-44] feat: todo content update 기능 추가
    
    * [BD-44] feat: todo isCompleted 수정 기능 추가

---
+++ client/src/components/Todo.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/Todo.module.css';
+import todoApi from '../api/todoApi';
+
+export default function Todo({ bucketId, todoId, todoContent, todoCompleted, fetchTodo }) {
+  const [content, setContent] = useState(todoContent);
+  const [isCompleted, setCompleted] = useState(todoCompleted);
+
+  useEffect(() => {
+    async function updateContent() {
+      try {
+        const formData = new FormData();
+        formData.append('isCompleted', isCompleted);
+        const response = await todoApi.updateTodo(bucketId, todoId, formData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    updateContent();
+  }, [isCompleted]);
+
+  useEffect(() => {
+    async function updateContent() {
+      try {
+        const formData = new FormData();
+        formData.append('content', content);
+        const response = await todoApi.updateTodo(bucketId, todoId, formData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    updateContent();
+  }, [content]);
+
+  function handleChangeCheckbox(e) {
+    if (e.target.checked) {
+      setCompleted(true);
+    } else {
+      setCompleted(false);
+    }
+  }
+
+  function handleChangeInput(e) {
+    setContent(e.target.value);
+  }
+
+  const handleDelete = async () => {
+    try {
+      await todoApi.deleteTodo(bucketId, todoId);
+      fetchTodo();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className={styles.todo}>
+      <input type="checkbox" onChange={handleChangeCheckbox} />
+
+      <input
+        id="content"
+        name="content"
+        type="text"
+        placeholder="투두 리스트 내용을 입력해주세요"
+        required
+        value={content}
+        onChange={handleChangeInput}
+      />
+
+      <button className={styles.deleteButton} onClick={handleDelete}>
+        X
+      </button>
+    </div>
+  );
+}
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -5,8 +5,7 @@ import Home from './Home';
 function Main() {
   const [isLogin, setIsLogin] = useState(false);
   // isLogin 은 나중에 redux 로 받은 isLogin값을 받아 화면을 보여줄 예정
-  // return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
-  return <Home />;
+  return <div>{isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />}</div>;
 }
 
 export default Main;
