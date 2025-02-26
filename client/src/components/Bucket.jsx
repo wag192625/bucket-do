ebb0fdf730175933ed45bd55132134f61b07bf4b
성준 <130916219+sungjoon92@users.noreply.github.com>
2025-02-26 13:06:13 +0900

    [BD-113] feat: 버킷 컴포넌트 기능구현 (#75)
    - 버킷 컴포넌트 titile, 이미지 api연결중

--- client/src/components/Bucket.jsx
+++ client/src/components/Bucket.jsx
@@ -3,17 +3,24 @@ import styles from '../styles/bucket.module.css';
 import bucketApi from '../api/bucketApi';
 import { useRef } from 'react';
 
-const INITIAL_FORM_DATA = {
-  title: '',
-  file: null,
-};
-
 function Bucket({ activeIndex, bucket }) {
-  const [title, setTitle] = useState('');
-  const [imagePath, setImagePath] = useState('');
-  const fileInputRef = useRef(null); // 파일 input 요소에 대한 참조
+  const [inputData, setInputData] = useState({
+    title: '',
+    file: '',
+  });
+  const fileInputRef = useRef(null);
 
-  const [inputData, setInputData] = useState(INITIAL_FORM_DATA);
+  // 버킷 초기화
+  useEffect(() => {
+    if (bucket) {
+      setInputData({
+        title: bucket.title || '',
+        file: bucket.imageUrl || '',
+      });
+    }
+
+    console.log(bucket);
+  }, [bucket]);
 
   const handleFormChange = (e) => {
     const { name, value } = e.target;
@@ -21,17 +28,17 @@ function Bucket({ activeIndex, bucket }) {
   };
 
   const handleFileChange = (e) => {
-    const file = e.target.files[0]; // 선택된 첫 번째 파일 가져오기
-    setInputData((prev) => ({ ...prev, file })); // 파일 state 업데이트
+    const file = e.target.files[0];
+    console.log(file);
+    setInputData((prev) => ({ ...prev, file }));
+
+    fileInputRef.current.value = file;
+    console.log(fileInputRef.current.value);
   };
 
-  const handleUpdateTitle = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(); // FormData 객체 생성
-
+  const handleAutoSubmit = async () => {
+    const formData = new FormData();
     formData.append('title', inputData.title);
-    formData.append('content', inputData.content);
 
     // 이미지 파일이 있는 경우에 폼데이터에 추가
     if (inputData.file) {
@@ -39,55 +46,23 @@ function Bucket({ activeIndex, bucket }) {
     }
 
     try {
-      await bucketApi.updateBucket(bucket.id, formData);
-      resetForm();
+      const response = await bucketApi.updateBucket(bucket.id, formData);
+      console.log(response);
+      console.log('✅ 자동 업데이트 성공!');
     } catch (error) {
-      console.error('ERROR : ', error);
+      console.error('❌ 업데이트 실패 : ', error);
     }
   };
 
-  // const handleUpdateTitle = async (e) => {
-  //   try {
-  //     setTitle(e.target.value);
-
-  //     const formData = new FormData();
-  //     formData.append('title', title);
-  //     if (inputData.file) {
-  //       formData.append('file', inputData.file);
-  //     }
-  //     const response = await bucketApi.updateBucket(bucket.id, formData);
-  //     // setTitle(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const handleUpdateImage = async (e) => {
-  //   const file = e.target.files[0]; // 선택된 첫 번째 파일 가져오기
-  //   setInputData((prev) => ({ ...prev, file })); // 파일 state 업데이트
-
-  //   const formData = new FormData(); // FormData 객체 생성
-  //   formData.append('title', 'dddd');
-
-  //   // 선택된 이미지 파일이 있으면 FormData 객체에 추가
-  //   if (inputData.file) {
-  //     formData.append('file', inputData.file);
-  //   }
-
-  //   try {
-  //     const response = await bucketApi.updateBucket(bucket.id, formData);
-  //   } catch (error) {
-  //     console.error('error:', error);
-  //   }
-  // };
+  useEffect(() => {
+    handleAutoSubmit();
+  }, [inputData]);
 
   return (
     <article className={styles.bucketItem}>
-      {/* {image && (
-        <div className={styles.bucketImageBox}>
-          <img className={styles.bucketImage} src={image} alt="미리보기" />
-        </div>
-      )} */}
+      <div className={styles.bucketImageBox}>
+        <img className={styles.bucketImage} src={bucket.imageUrl} alt="미리보기" />
+      </div>
 
       <form className={styles.bucketForm}>
         <input
@@ -96,7 +71,6 @@ function Bucket({ activeIndex, bucket }) {
           accept="image/*"
           ref={fileInputRef}
           name="image_path"
-          value={imagePath}
           onChange={handleFileChange}
         />
         <input
