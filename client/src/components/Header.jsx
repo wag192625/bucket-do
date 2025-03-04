afc1b33a816fad7b1da9fa6c0ccd74b4af64f325
subniii <144028057+imdla@users.noreply.github.com>
2025-03-04 19:13:17 +0900

    feat: 로그아웃 기능 구현 (#124)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
+import authApi from '../api/authApi';
 import { logout } from '../store/slices/authSlice';
 import styles from '../styles/components/Header.module.css';
 
 import Modal from './Modal';
+import errorMessages from '../config/errorMessages';
 
 function Header() {
   const dispatch = useDispatch();
@@ -22,9 +24,32 @@ function Header() {
     setModalData({
       ...modalData,
       content: '로그아웃하시겠습니까?',
+      confirmText: '확인',
+      cancelText: '취소',
+      onConfirm: async () => await authLogout(),
     });
     setIsModalOpen(true);
   }
+
+  // 사용자 로그아웃
+  const authLogout = async () => {
+    setIsModalOpen(false);
+
+    const response = await authApi.logout();
+    if (response.status === 204) {
+      dispatch(logout());
+    } else {
+      const errorMessage =
+        errorMessages[error.status]?.[error.code] || errorMessages[error.status]?.DEFAULT;
+      const modalData = {
+        content: errorMessage,
+        cancelText: '확인',
+        onConfirm: false,
+      };
+
+      modalOpen(modalData);
+    }
+  };
 
   return (
     <header>
